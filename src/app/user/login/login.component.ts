95a1fc1a72a4869749ac60a173c9fc16d9a3d510
Hamza <hamzadanden2@gmail.com>
2020-01-15 17:18:31 +0100

    login almost there need to save token in the local storage

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from "@angular/forms";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  
+  form: FormGroup
+  constructor(public user: FormBuilder, private http: HttpClient) {
+    this.form = this.user.group({
+      
+      email: [''],
+      password: ['']
+    })
+  }
 
   ngOnInit() {
   }
+
+  submitForm(){
+    
+
+    
+    var obj = {
+      email: this.form.get('email').value,
+      password: this.form.get('password').value,
+
+    }
+
+    return this.http.post('http://localhost:8080/api/user/login', obj).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    )
+
+  }
+
+  
 
 }
--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from "@angular/forms";
-import { AppComponent } from './../../app.component'
 
 @Component({
   selector: 'app-sign-up',
@@ -26,22 +25,14 @@ export class SignUpComponent implements OnInit {
   
 
   submitForm() {
-
-    var formData: any = new FormData();
-    formData.append("name", this.form.get('name').value);
-    formData.append("email", this.form.get('email').value);
-    formData.append("password", this.form.get('password').value);
-    formData.append("confirmedPassword", this.form.get('confirmedPassword').value);
-
-    
     var obj = {
-      name: formData.getAll('name')[0],
-      email: formData.getAll('email')[0],
-      password: formData.getAll('password')[0],
-      confirmedPassword: formData.getAll('confirmedPassword')[0],
+      name: this.form.get('name').value,
+      email: this.form.get('email').value,
+      password: this.form.get('password').value,
+      confirmedPassword: this.form.get('confirmedPassword').value,
 
     }
-    // console.log(obj.confirmePassword)
+    
    return this.http.post('http://localhost:8080/api/user/register', obj).subscribe(
       (response) => console.log(response),
       (error) => console.log(error)
