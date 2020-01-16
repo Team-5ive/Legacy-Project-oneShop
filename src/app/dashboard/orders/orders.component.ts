3c0155323d304a5b14612c1bc35875cea7e1d0a4
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2020-01-16 19:02:43 +0100

    Orders Complete

--- src/app/dashboard/orders/orders.component.ts
+++ src/app/dashboard/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    return this.http.get('http://localhost:8080/api/orders').subscribe(
+      (response: any[]) => {
+        var results = []
+        for (var i = 0; i < response.length; i++) {
+          results.push([{ "total_order_price": response[i]["total_order_price"] }, { "payment_method": response[i]["payment_method"] }, { "status": response[i]["status"] }, { "title": response[i]["products"][0]["product"]["title"] }])
+        }
+        console.log(results)
+      })
   }
 
 }
