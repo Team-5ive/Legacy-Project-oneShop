853a09498aec23b422f9965673c7650711ca6167
SofianSaleh <sofiane.saleh11@gmail.com>
2020-01-19 18:04:19 +0100

    almost finished

--- src/app/dashboard/statistics/statistics.component.ts
+++ src/app/dashboard/statistics/statistics.component.ts
@@ -9,36 +9,84 @@ import { HttpClient } from "@angular/common/http";
 export class StatisticsComponent implements OnInit {
   users;
   userLogs = [];
-  j: number = 0;
+  orders;
+  allProducts;
+  obj: object = {};
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.getOrder();
     this.http.get("http://localhost:8080/api/users").subscribe(data => {
       this.users = data;
-      this.getOrders();
+      for (let i = 0; i < this.users.length; i++) {
+        this.getOrders(i);
+      }
+      setTimeout(() => {
+        this.sort();
+      }, 1000);
     });
   }
 
-  getOrders() {
-    for (var i = 0; i < this.users.length; i++) {
-      this.j = i;
-      console.log(this.users);
-      this.http
-        .get(`http://localhost:8080/api/orders/${this.users[this.j]._id}`)
-        .subscribe(results => {
-          console.log(this.j);
-          // console.log(this.users[this.j]);
-          // console.log(results);
-          this.userLogs.push({
-            username: this.users[this.j]["name"],
-            orders: results
-          });
-          console.log(this.userLogs);
+  getOrders(i: number) {
+    this.http
+      .get(`http://localhost:8080/api/orders/${this.users[i]._id}`)
+      .subscribe(results => {
+        this.userLogs.push({
+          username: this.users[i]["name"],
+          orders: results
         });
-    }
+        // console.log(this.userLogs);
+      });
+    // return;
+  }
+
+  sort() {
     this.userLogs = this.userLogs.sort(
       (a, b) => b["orders"].length - a["orders"].length
     );
+    console.log(this.userLogs);
+  }
+
+  getOrder() {
+    this.http.get(`http://localhost:8080/api/orders`).subscribe(data => {
+      this.orders = data;
+
+      this.getProducts();
+    });
+  }
+
+  getProducts() {
+    this.http
+      .get("http://localhost:8080/api/allproducts")
+      .subscribe(response => {
+        // alert("console just logged all products");
+        this.allProducts = response;
+        this.compare();
+      });
+  }
+
+  compare() {
+    // console.log(this.allProducts);
+    // console.log(this.orders);
+    // for (var m = 0; m < this.allProducts.length; m++) {
+
+    // }
+
+    for (let j = 0; j < this.orders.length; j++) {
+      for (let k = 0; k < this.orders[j].products.length; k++) {
+        // console.log(this.orders[j].products[1]);
+        // for (let l = 0; l < this.orders[j].products[l].length; l++) {
+        // console.log(this.orders[j].products[k].product.title);
+        if (this.obj[this.orders[j].products[k].product.title] === undefined) {
+          this.obj[`${this.orders[j].products[k].product.title}`] = 0;
+        } else {
+          this.obj[this.orders[j].products[k].product.title] += 1;
+        }
+        // }
+      }
+    }
+    console.log(this.obj);
+    // this.obj = this.obj.sort((a,b) => )
   }
 }
