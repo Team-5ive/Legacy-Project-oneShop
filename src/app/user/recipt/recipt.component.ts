8af8017f0bcada535367ef505aa21e84956e279f
SofianSaleh <sofiane.saleh11@gmail.com>
2020-01-19 14:49:00 +0100

    made download button

--- src/app/user/recipt/recipt.component.ts
+++ src/app/user/recipt/recipt.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { VariablesService } from "./../../variables.service";
-
+import * as jsPDF from "jspdf";
 @Component({
   selector: "app-recipt",
   templateUrl: "./recipt.component.html",
@@ -8,10 +8,29 @@ import { VariablesService } from "./../../variables.service";
 })
 export class ReciptComponent implements OnInit {
   obj;
+
+  @ViewChild("content", { static: false }) content: ElementRef;
+
   constructor(private variable: VariablesService) {}
 
   ngOnInit() {
     this.obj = JSON.parse(this.variable.checkoutInfo);
     console.log(this.obj);
+  }
+
+  downloadPDF() {
+    let doc = new jsPDF();
+
+    let specialElementsHandlers = {
+      "#editor": function(element, renderer) {
+        return true;
+      }
+    };
+    let content = this.content.nativeElement;
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      width: 190,
+      elementntHandlers: specialElementsHandlers
+    });
+    doc.save("recipt.pdf");
   }
 }
